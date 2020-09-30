702d1517dc77acbd72d0ecf6ed037c2a0bd3d6a7
christianwcabrerav <catp.ccabrerav@gmail.com>
2020-09-29 19:33:55 -0500

    Mi primer commit

---
+++ src/demo/Ejecucion.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		System.out.println(x);
+
+		System.out.println(y);
+
+		System.out.println(z);
+
+	}
+
+}
